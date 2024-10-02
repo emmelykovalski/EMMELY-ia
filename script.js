bea463661d3d9ef08d56af9d0eb754618678bf81
emmelykovalski <107261218+emmelykovalski@users.noreply.github.com>
2024-10-02 11:22:36 -0300

    02/10

---
+++ script.js
@@ -0,0 +1,50 @@
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+const caixaAlternativa = document.querySelector(".caixa-Alternativas");
+const caixaResultados= document.querySelector(".caixa-Resultados");
+const textoResultados = document.querySelector(".texto-Resultados");
+
+
+const Perguntas = [
+{
+    enunciado :"Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
+ 
+
+    alternativas: [
+        "isso é assustador!",
+        "isso é maravilhoso!",
+    ]
+
+
+
+
+},
+
+{
+    enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
+
+alternativas : [
+"Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
+
+]
+
+
+},
+
+{
+    enunciado: "Depois que Gabriel escreveu o trabalho, teve uma discussão sobre o impacto da IA no trabalho do futuro o que Gabriel faz:",
+
+    alternativas: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
+    "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+
+},
+
+
+
+
+
+
+
+]
+
